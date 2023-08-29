b808de64111e1bc95382b19c0382c3c60dcb2502
badman-dev <budmandev@gmail.com>
2023-08-29 15:22:45 -0400

    added previous assignment files

---
+++ src/ProductReader.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class ProductReader {
+    public static void main(String[] args)
+    {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+
+        try
+        {
+            // uses a fixed known path:
+            //  Path file = Paths.get("c:\\My Documents\\data.txt");
+
+            // use the toolkit to get the current working directory of the IDE
+            // Not sure if the toolkit is thread safe...
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+            // Typiacally, we want the user to pick the file so we use a file chooser
+            // kind of ugly code to make the chooser work with NIO.
+            // Because the chooser is part of Swing it should be thread safe.
+            chooser.setCurrentDirectory(workingDirectory);
+            // Using the chooser adds some complexity to the code.
+            // we have to code the complete program within the conditional return of
+            // the filechooser because the user can close it without picking a file
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                // Typical java pattern of inherited classes
+                // we wrap a BufferedWriter around a lower level BufferedOutputStream
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+                // Finally we can read the file LOL!
+                System.out.printf( "%-10s %-15s %-30s %-15s %n", "ID#", "Name", "Description", "Cost");
+                System.out.println("======================================================================");
+
+                int line = 0;
+                while(reader.ready())
+                {
+                    rec = reader.readLine();
+                    String[] recSplit = rec.split(",");
+                    line++;
+                    // echo to screen
+                    System.out.printf("\n%-10s %-15s %-30s %-15s", recSplit[0], recSplit[1], recSplit[2], recSplit[3]);
+                }
+                reader.close(); // must close the file to seal it and flush buffer
+                System.out.println("\n\nData file read!");
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
